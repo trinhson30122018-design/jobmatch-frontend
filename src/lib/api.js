e0a79d3fc628816be824fe4b527386a4caf95c14
trinhson30122018 <trinhson301293@gmail.com>
2026-03-18 19:23:45 +0700

    fix api base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE =
   import.meta.env.VITE_API_BASE ||
-  "https://jobmatch-backend-1-lrv0.onrender.com";
+  "https://jobmatch-backend-1-lrv0.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE,
