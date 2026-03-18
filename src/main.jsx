9428326078b3bb5dcc76ee73acaab7f45ae4c49e
trinhson30122018 <trinhson301293@gmail.com>
2026-03-18 19:12:51 +0700

    fix css conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -9,15 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 4699bdc9cb3ce5c4fbe1e3a36aa000ad8fc3594d
